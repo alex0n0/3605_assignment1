015061d2e87a64a63966cf38f2fbd18a555e5c07
alex0n0 <42526086+alex0n0@users.noreply.github.com>
2018-08-30 00:08:42 +1000

    game1

---
+++ 1_roller_ball/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    private int score;
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        score = 0;
+        GetScore();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+   
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            score++;
+            GetScore();
+        }
+    }
+
+    void GetScore()
+    {
+        countText.text = "Count: " + score.ToString();
+        if (score >= 10)
+        {
+            winText.text = "you win";
+        }
+    }
+}
